0443651fba383c82845ce8e493e5eff291288e8c
ShinraTenseiiiii <pandeyjoydeep@gmail.com>
2024-07-15 13:42:40 +0530

    made it responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, AboutComponent, ContactComponent],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,8 @@ import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
 
-{ path: '', redirectTo: '/home', pathMatch: 'full' },
-{ path: 'home', component: HomeComponent },
+{ 'path': '', redirectTo: '/home', pathMatch: 'full' },
+{ 'path': 'home', component: HomeComponent },
 { 'path' : 'about' , component: AboutComponent },
 { 'path' : 'contact' , component:ContactComponent },
 
